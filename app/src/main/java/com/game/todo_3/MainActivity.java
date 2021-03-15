56c71d95ab6da5be1a2b21306e53b67be7c66dd9
kushal-7 <chhetrikushal14@gmail.com>
2021-03-16 04:27:09 +0600

    first commit

---
+++ app/src/main/java/com/game/todo_3/MainActivity.java
@@ -0,0 +1,23 @@
+package com.game.todo_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void next(View view) {
+    Intent obj= new Intent(this, SecondMainActivity.class);
+    startActivity(obj);
+    }
+
+
+}
